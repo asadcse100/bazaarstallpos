c9684772f2550dfb720734edb9bcf4f120c70b2d
asadcse100 <asadcse100@gmail.com>
2023-10-26 08:03:26 +0600

    uu

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -142,98 +142,12 @@ class AppServiceProvider extends ServiceProvider
         // });
 
         //Blade directive to format quantity values into required format.
-        Blade::directive('format_quantity', function ($expression) {
-            return "number_format($expression, session('business.quantity_precision', 2), session('currency')['decimal_separator'], session('currency')['thousand_separator'])";
-        });
+        // Blade::directive('format_quantity', function ($expression) {
+        //     return "number_format($expression, session('business.quantity_precision', 2), session('currency')['decimal_separator'], session('currency')['thousand_separator'])";
+        // });
 
         //Blade directive to return appropiate class according to transaction status
-        Blade::directive('transaction_status', function ($status) {
-            return "<?php if($status == 'ordered'){
-                echo 'bg-aqua';
-            }elseif($status == 'pending'){
-                echo 'bg-red';
-            }elseif ($status == 'received') {
-                echo 'bg-light-green';
-            }?>";
-        });
-
-        //Blade directive to return appropiate class according to transaction status
-        Blade::directive('payment_status', function ($status) {
-            return "<?php if($status == 'partial'){
-                echo 'bg-aqua';
-            }elseif($status == 'due'){
-                echo 'bg-yellow';
-            }elseif ($status == 'paid') {
-                echo 'bg-light-green';
-            }elseif ($status == 'overdue') {
-                echo 'bg-red';
-            }elseif ($status == 'partial-overdue') {
-                echo 'bg-red';
-            }?>";
-        });
-
-        //Blade directive to display help text.
-        Blade::directive('show_tooltip', function ($message) {
-            return "<?php
-                if(session('business.enable_tooltip')){
-                    echo '<i class=\"fa fa-info-circle text-info hover-q no-print \" aria-hidden=\"true\" 
-                    data-container=\"body\" data-toggle=\"popover\" data-placement=\"auto bottom\" 
-                    data-content=\"' . $message . '\" data-html=\"true\" data-trigger=\"hover\"></i>';
-                }
-                ?>";
-        });
-
-        //Blade directive to convert.
-        Blade::directive('format_date', function ($date) {
-            if (! empty($date)) {
-                return "\Carbon::createFromTimestamp(strtotime($date))->format(session('business.date_format'))";
-            } else {
-                return null;
-            }
-        });
-
-        //Blade directive to convert.
-        Blade::directive('format_time', function ($date) {
-            if (! empty($date)) {
-                $time_format = 'h:i A';
-                if (session('business.time_format') == 24) {
-                    $time_format = 'H:i';
-                }
-
-                return "\Carbon::createFromTimestamp(strtotime($date))->format('$time_format')";
-            } else {
-                return null;
-            }
-        });
-
-        Blade::directive('format_datetime', function ($date) {
-            if (! empty($date)) {
-                $time_format = 'h:i A';
-                if (session('business.time_format') == 24) {
-                    $time_format = 'H:i';
-                }
-
-                return "\Carbon::createFromTimestamp(strtotime($date))->format(session('business.date_format') . ' ' . '$time_format')";
-            } else {
-                return null;
-            }
-        });
-
-        //Blade directive to format currency.
-        Blade::directive('format_currency', function ($number) {
-            return '<?php 
-            $formated_number = "";
-            if (session("business.currency_symbol_placement") == "before") {
-                $formated_number .= session("currency")["symbol"] . " ";
-            } 
-            $formated_number .= number_format((float) '.$number.', session("business.currency_precision", 2) , session("currency")["decimal_separator"], session("currency")["thousand_separator"]);
-
-            if (session("business.currency_symbol_placement") == "after") {
-                $formated_number .= " " . session("currency")["symbol"];
-            }
-            echo $formated_number; ?>';
-        });
-
+       
         $this->registerCommands();
     }
 
